3130906915be675400e3f90b92e0b52c954df478
Jahongir <alimovj727@gmail.com>
2025-03-16 06:26:52 +0500

    Create SendWelcomeEmail listener

--- database/migrations/2025_03_15_125338_create_posts_table.php
+++ database/migrations/2025_03_15_125338_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
            $table->string('slug')->unique();
-           $table->foreignId('user_id')->constrained()->onDelete('cascade');
+             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
