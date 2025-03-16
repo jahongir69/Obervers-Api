6245c476f64b5c9549951a1f428732da863ad03b
Jahongir <alimovj727@gmail.com>
2025-03-16 17:08:18 +0500

    Improve GitHub Actions for CI/CD pipeline

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use App\Observers\PostObserver;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+
+#[ObservedBy([PostObserver::class])]
 
 class Post extends Model
 {
@@ -13,9 +16,4 @@ class Post extends Model
     protected $fillable = ['title', 'slug', 'description', 'user_id'];
 
     
-    protected static function boot()
-    {
-        parent::boot();
-        static::observe(PostObserver::class);
-    }
 }
